70dfb307bfa91468f77ee979942ed2915b5f0c87
chickie8x <chickie8x.qn@gmail.com>
2023-04-01 18:34:52 +0700

    save state

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const boardSize = 9
 
     const gameBoard = document.getElementById('gameboard')
+    const info = document.getElementById('info')
 
     for (let i = 0; i < boardSize; i++) {
         const cell = document.createElement('div')
@@ -12,12 +13,59 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     const cells = document.getElementsByClassName('square')
-    console.log(cells)
-    for (cell of cells){
-        cell.addEventListener('click', function(e) {
-            console.log(e.target.id)
-        })
+
+    let turn = 'circle'
+
+    let currentCell = undefined
+
+    let message = `${turn} move`
+
+    info.innerHTML = message
+
+    const wincells = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 6, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    function checkClassName(arr){
+        console.log(arr)
+        arr.map(item => console.log(cells[item].dataset.move))
     }
 
+    function checkWin() {
+        for(item of wincells){
+           checkClassName(item)
+        }
+    }
+
+    function addMove() {
+        currentCell = this
+        const cirle = document.createElement('div')
+        cirle.classList.add(turn)
+        currentCell.append(cirle)
+        currentCell.dataset.move = turn
+        currentCell.removeEventListener('click', addMove)
+        if (turn === 'circle') {
+            turn = 'cross'
+        }
+        else {
+            turn = 'circle'
+        }
+        message = `${turn} move`
+        info.innerHTML = message
+        checkWin()
+    }
+
+    for (cell of cells) {
+        cell.addEventListener('click', addMove)
+    }
+
+    
 
 })
